f9d56a11e9eba7844f581e65b4d6e82dfe84172e
Lukas Waymann <meribold@gmail.com>
2016-05-15 00:22:58 +0200

    Update task1/src/Module.mk

--- task1/src/Module.mk
+++ task1/src/Module.mk
@@ -1,20 +1,22 @@
 local_program := $(subdirectory)/task1
-local_sources := $(shell find $(subdirectory) -maxdepth 1 -name '*.cpp')
 
-sources       += $(local_sources)
-programs      += $(local_program)
+sources  += $(addsuffix .cpp,$(local_program))
+programs += $(local_program)
 
 $(local_program) : all_ldflags  = $(addprefix -L,$(ld_dirs)) $(LDFLAGS)
 $(local_program) : all_ldlibs   = $(LDLIBS) -lboost_regex \
    $(shell icu-config --ldflags-libsonly) \
    $(patsubst lib%.a,-l%,$(notdir $(libraries)))
 
-# Beware: While variables appearing in the target and prerequisite parts of a rule are
+# Enable the second expansion of prerequisites (only).
+.SECONDEXPANSION:
+
+# Beware: while variables appearing in the target and prerequisite parts of a rule are
 # immediately evaluated, inside recipes, expanding "occurs after make has finished reading
 # all the makefiles and the target is determined to be out of date"
-# (http://www.gnu.org/software/make/manual/make.html#Variables-in-Recipes). This means we
+# (http://www.gnu.org/software/make/manual/make.html#Variables-in-Recipes).  This means we
 # can't use the `local_` variables in the recipe.
-$(local_program): $(local_sources:.cpp=.o) $(libraries)
+$(local_program): $(addsuffix .o,$(local_program)) $$(libraries)
 	$(CXX) $(all_ldflags) $^ $(all_ldlibs) -o $@
 
 # vim: tw=90 ts=8 sts=-1 sw=3 noet
