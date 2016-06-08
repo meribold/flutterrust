d424a8ee5858ee7204f03f7b3884562a11736256
Lukas Waymann <meribold@gmail.com>
2016-06-08 21:24:26 +0200

    Finish task 2 (hopefully)

--- task2/src/bitmap.cpp
+++ task2/src/bitmap.cpp
@@ -2,8 +2,8 @@
 
 #include <cstdint>   // uint8_t, uint16_t
 #include <fstream>   // ifstream
-#include <iostream>  // TODO: remove.
 #include <stdexcept> // runtime_error
+// #include <iostream>
 
 using std::uint8_t;
 using std::uint16_t;
@@ -36,11 +36,12 @@ Bitmap::Bitmap(const std::string& path) {
    }
    TargaHeader targaHeader;
    iS.read(reinterpret_cast<char*>(&targaHeader), sizeof(targaHeader));
+   auto imageSpec = targaHeader.imageSpecification;
+
+   /*
    auto colorMapSpecification = targaHeader.colorMapSpecification;
    auto cMSFirstEntryIndex = colorMapSpecification.firstEntryIndex;
-   auto imageSpec = targaHeader.imageSpecification;
    auto imageDescriptor = imageSpec.imageDescriptor;
-
    std::cerr << "iDLength: " << static_cast<int>(targaHeader.iDLength) << '\n'
              << "colorMapType: " << static_cast<int>(targaHeader.colorMapType) << '\n'
              << "imageType: " << static_cast<int>(targaHeader.imageType) << '\n'
@@ -50,22 +51,31 @@ Bitmap::Bitmap(const std::string& path) {
              << "width: " << static_cast<int>(imageSpec.width) << '\n'
              << "height: " << static_cast<int>(imageSpec.height) << '\n'
              << "pixelDepth: " << static_cast<int>(imageSpec.pixelDepth) << '\n'
-             << "imageDescriptor: " << static_cast<int>(imageDescriptor)
-             << std::endl;
+             << "imageDescriptor: " << static_cast<int>(imageDescriptor) << std::endl;
+   */
 
    // Skip the optional image ID field.  Not technically required for the assignment:
    // it's guaranteed to be zero for all TGAs we need to support.
    iS.ignore(targaHeader.iDLength);
    // Skip the color map data.  Not technically required for the same reason.
    iS.ignore(targaHeader.colorMapSpecification.length);
-   // Get the pixel data.
+
+   numRows = imageSpec.width; numCols = imageSpec.height;
+   std::streamsize numPixels = numRows * numCols;
+   // quadlets = decltype(quadlets){new RGBAQuadlet[numPixels]};
+   quadlets = std::make_unique<RGBAQuadlet[]>(numPixels);
+
+   // Now get the pixel data.
    switch (imageSpec.pixelDepth) {
-      case 24:
+      case 24: {
+         for (decltype(numPixels) i = 0; i < numPixels; ++i) {
+            iS.read(reinterpret_cast<char*>(&quadlets[i]), 3);
+            quadlets[i][3] = 0xff;
+         }
          break;
+      }
       case 32: {
-         std::streamsize numBytes = 4 * imageSpec.width * imageSpec.height;
-         quadlets.reserve(numBytes);
-         iS.read(reinterpret_cast<char*>(quadlets.data()), numBytes);
+         iS.read(reinterpret_cast<char*>(quadlets.get()), 4 * numPixels);
          break;
       }
       default:
@@ -73,26 +83,32 @@ Bitmap::Bitmap(const std::string& path) {
    }
 }
 
-const RGBAQuadlet& Bitmap::operator()(int row, int col) const {
-   return quadlets[32 * row + col];
+const RGBAQuadlet* Bitmap::operator[](int row) const {
+   return &quadlets[numCols * row];
 }
 
+const RGBAQuadlet& Bitmap::operator()(int row, int col) const {
+   // TODO: this version could perform bounds checking.
+   return quadlets[numCols * row + col];
+}
+
+// Get an ASCII character representing the intensity of the pixel.
 char getGlyph(const RGBAQuadlet& quadlet) {
    static constexpr std::size_t size = 11;
    static constexpr auto divident = 255 * 3 * 255;
-   static std::array<char, size> shadeGlyphs{
+   static constexpr std::array<char, size> shadeGlyphs{{
       ' ', '.', ':', '-', '~', '"', '+', 'o', '*', '%', '#'
-   };
+   }};
    std::size_t index = (quadlet[0] + quadlet[1] + quadlet[2]) * quadlet[3] * (size - 1) /
       divident;
    return shadeGlyphs.at(index);
 }
 
-// TODO.
+// Output an ASCII representation of the bitmap.
 std::ostream& operator<<(std::ostream& oS, const Bitmap& bitmap) {
-   for (int row = 31; row >= 0; --row) {
-      for (int col = 0; col < 32; ++col) {
-         auto glyph = getGlyph(bitmap(row, col));
+   for (int row = bitmap.numRows - 1; row >= 0; --row) {
+      for (std::size_t col = 0; col < bitmap.numCols; ++col) {
+         auto glyph = getGlyph(bitmap[row][col]);
          oS << glyph << glyph;
       }
       oS << std::endl;
--- task2/src/bitmap.hpp
+++ task2/src/bitmap.hpp
@@ -4,11 +4,12 @@
 #include <array>
 #include <climits> // CHAR_BIT
 #include <cstdint> // uint8_t
-#include <vector>
+#include <memory>  // unique_ptr
 
 #include <boost/detail/endian.hpp>  // BOOST_LITTLE_ENDIAN
 
-// Assert some basic things about the target machine at compile-time.
+// Assert some basic things about the target machine at compile-time.  Prudence, not
+// doubt.
 static_assert (CHAR_BIT == 8, "The Bitmap class relies on bytes having 8 bits");
 #ifndef BOOST_LITTLE_ENDIAN
    #error The Bitmap class relies on little-endian byte order being used
@@ -18,10 +19,12 @@ using RGBAQuadlet = std::array<std::uint8_t, 4>;
 
 struct Bitmap {
    Bitmap(const std::string& path);
+   const RGBAQuadlet* operator[](int row) const;
    const RGBAQuadlet& operator()(int row, int col) const;
    friend std::ostream& operator<<(std::ostream&, const Bitmap&);
   private:
-   std::vector<RGBAQuadlet> quadlets;
+   std::size_t numRows, numCols;
+   std::unique_ptr<RGBAQuadlet[]> quadlets;
 };
 
 #endif // BITMAP_HPP_VEAHQYJM
